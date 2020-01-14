0360180eab585300047f4223e71b11c1624f004d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-14 23:36:43 +0000

    r Reformat more code

--- tests/catch2_build_tests/main.cpp
+++ tests/catch2_build_tests/main.cpp
@@ -2,4 +2,5 @@
 #include "ApprovalTests/integrations/catch/Catch2Approvals.h"
 #include "ApprovalTests/Approvals.h"
 
-auto directory = ApprovalTests::Approvals::useApprovalsSubdirectory("approval_tests");
+auto directory =
+    ApprovalTests::Approvals::useApprovalsSubdirectory("approval_tests");
--- tests/doctest_build_tests/main.cpp
+++ tests/doctest_build_tests/main.cpp
@@ -2,4 +2,5 @@
 #include "ApprovalTests/integrations/doctest/DocTestApprovals.h"
 #include "ApprovalTests/Approvals.h"
 
-auto directory = ApprovalTests::Approvals::useApprovalsSubdirectory("approval_tests");
+auto directory =
+    ApprovalTests::Approvals::useApprovalsSubdirectory("approval_tests");
